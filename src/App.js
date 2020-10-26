3ffcfbc2ed0c6f5282c67e657ddfebd3ba3b6b84
Franco Ortega <francopdx@gmail.com>
2020-10-26 16:16:21 -0700

    add Title.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Header.js';
+import Title from './Title.js';
 
 function App() {
   return (
     <div className="App">
         <Header />
+        <Title />
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import print from './assets/print-icon.png';
+
+export default class Title extends Component {
+    render() {
+        return (
+            <div>
+                <div className='title-text'>
+                    Chocolate Pizza
+                </div>
+                <div className='subtitle-text'>
+                    POSTED ON 15 DEC 2013/DESSERTS
+                </div>
+                <div className="print-div">
+                    <img src={print} alt='print icon' />
+                    PRINT
+                </div>
+            </div>
+        )
+    }
+}
