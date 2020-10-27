676d399db1a15b56f5a51252ac11a68109f38986
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-10-26 17:51:47 -0700

    title alignment

--- src/Title.js
+++ src/Title.js
@@ -4,12 +4,14 @@ import print from './assets/print-icon.png';
 export default class Title extends Component {
     render() {
         return (
-            <div>
-                <div className='title-text'>
-                    Chocolate Pizza
-                </div>
-                <div className='subtitle-text'>
-                    POSTED ON 15 DEC 2013/DESSERTS
+            <div className='title-div'>
+                <div className='left-title-div'>
+                    <div className='title-text'>
+                        Chocolate Pizza
+                    </div>
+                    <div className='subtitle-text'>
+                        POSTED ON 15 DEC 2013/DESSERTS
+                    </div>
                 </div>
                 <div className="print-div">
                     <img src={print} alt='print icon' />
